d1285071f6f209577c07cb69b2a7012170db41d3
Kieron Lanning <kieronlanning@users.noreply.github.com>
2026-08-01 08:24:29 +0100

    fix: sample startup failed with connection string

--- src/src/Samples.AppHost/Program.cs
+++ src/src/Samples.AppHost/Program.cs
@@ -1,6 +1,4 @@
 using Aspire.Hosting.ApplicationModel;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Purview.EventSourcing.Samples;
 using Purview.EventSourcing.Samples.AppHost.AppModel;
 using Purview.EventSourcing.Samples.AppHost.AppModel.Resources;
@@ -10,20 +8,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 if (Environment.UserInteractive)
 	Console.Title = $"[{builder.Environment.EnvironmentName}] Samples.AppHost v{AssemblyInfo.Version}";
 
-builder
-	.Services.AddOptions<SampleAppHostKit.SampleAppHostKitOptions>()
-	.BindConfiguration(SampleAppHostKit.SampleAppHostKitOptions.SectionName)
-	.ValidateOnStart();
-
-var hostKitOptions =
-	builder
-		.Configuration.GetSection(SampleAppHostKit.SampleAppHostKitOptions.SectionName)
-		.Get<SampleAppHostKit.SampleAppHostKitOptions>()
-	?? new();
-var hostKit = new SampleAppHostKit(hostKitOptions);
-hostKit.Build(builder);
-hostKit.Configure();
-builder.Services.AddSingleton(hostKit);
+builder.AddAspireResourceKit();
+var hostKit = builder
+	.Services.Where(descriptor => descriptor.ServiceType == typeof(SampleAppHostKit))
+	.Select(descriptor => descriptor.ImplementationInstance)
+	.OfType<SampleAppHostKit>()
+	.Single();
 
 var variants = new[]
 {
