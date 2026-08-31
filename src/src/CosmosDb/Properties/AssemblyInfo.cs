c609f78ee0829d93ce9c78454b6d06b98b6490cd
Kieron Lanning <kieronlanning@users.noreply.github.com>
2026-08-31 22:43:49 +0100

    chore: moving pcs

--- src/src/CosmosDb/Properties/AssemblyInfo.cs
+++ src/src/CosmosDb/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 using Purview.Telemetry;
 
-[assembly: InternalsVisibleTo("SharedTestingFramework")]
-[assembly: InternalsVisibleTo("EventSourcing.UnitTests")]
+[assembly: InternalsVisibleTo("Purview.EventSourcing.SharedTestingFramework")]
+[assembly: InternalsVisibleTo("Purview.EventSourcing.UnitTests")]
 
 [assembly: ActivitySourceGeneration("Purview.EventSourcing.CosmosDb")]
 [assembly: MeterGeneration("Purview.EventSourcing.CosmosDb")]
